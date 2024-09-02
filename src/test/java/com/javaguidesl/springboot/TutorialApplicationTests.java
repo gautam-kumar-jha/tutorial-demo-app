3af4c479e2bf955c3e36c4aa6e9933065ee8d868
gautam-kumar-jha <gautamkumarjhagt@email.com>
2024-09-02 19:28:53 +0530

    added unit test

--- src/test/java/com/javaguidesl/springboot/TutorialApplicationTests.java
+++ src/test/java/com/javaguidesl/springboot/TutorialApplicationTests.java
@@ -1,13 +1,34 @@
 package com.javaguidesl.springboot;
 
+import com.javaguidesl.springboot.controller.ServerStatusController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 class TutorialApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+		private MockMvc mockMvc;
 
+		@InjectMocks
+		private ServerStatusController serverStatusController;
+
+		@BeforeEach
+		public void setUp() {
+			MockitoAnnotations.openMocks(this);
+			mockMvc = MockMvcBuilders.standaloneSetup(serverStatusController).build();
+		}
+
+		@Test
+		public void testServerStatus() throws Exception {
+			mockMvc.perform(get("/api/v1/server/status/")
+							.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk());
+		}
 }
